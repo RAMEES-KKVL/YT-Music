7576067fbcdc9bf29d5d9e4c979b309c1b51afad
RAMEES-KKVL <rameesp41750@gmail.com>
2024-05-14 22:08:11 +0530

    Completed login page design with some functionalities

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,8 +1,40 @@
 import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
     selector : "auth-login",
     templateUrl : "./login.component.html",
     styleUrls : ["./login.component.css"]
 })
-export class LoginBody{}
+export class LoginBody{
+    constructor(private http: HttpClient) {}
+
+    loginForm = {
+        email: "",
+        password: ""
+    }
+
+    onSubmit( formData: {email: string, password: string} ){
+        
+        this.http.post<loginResponse> ('http://localhost:7000/login', formData, httpOptions)
+        .subscribe({
+            next: response =>{
+                console.log(response);
+                
+            },
+            error: error =>{
+                console.log(error);
+                
+            }
+        })
+    }
+}
+
+const httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" })
+}
+
+interface loginResponse{
+    token: string 
+}
+
